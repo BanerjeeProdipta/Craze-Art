07bcdda5a1dabf66b7af276cb43f9b4f22c5ff84
Prodipta Banerjee <prodiptaj@live.com>
2023-01-05 20:39:36 +0600

    fix: git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7efe6453a6507a2"; }
+/******/ 		__webpack_require__.h = function() { return "299943364142587b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Layout from '../components/layout'
 import '../styles/global.css'
 import '../styles/tailwind.css'
 
 function App({ Component, pageProps }: any) {
+  // on refresh scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <>
       <Head>
@@ -12,6 +20,7 @@ function App({ Component, pageProps }: any) {
         </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ToastContainer />
       <Layout>
         <Component {...pageProps} />
       </Layout>
