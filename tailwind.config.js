246041ad7fcf49bbe3873aa51890253b860c023f
ProdiptaOneShotEarth <106379038+ProdiptaOneShotEarth@users.noreply.github.com>
2023-02-01 18:41:00 +0600

    feat(daisy ui): installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,37 @@
-const colors = require('tailwindcss/colors');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-    content: ["./pages/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}","./layouts/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                green: colors.emerald,
-                blue: colors.blue,
-                indigo: colors.indigo,
-                purple: colors.violet,
-                pink: colors.pink,
-                rose: colors.rose,
-                bgLeft: '#984D38',
-                bgRight: '#181E41',
-                primaryDark: '#671AE4',
-                primaryLight: '#B75CFF'
-            },
-            screens: {
-                'xs': '320px',
-            },
-            fontFamily: {
-                'sans': ['Inter', 'sans-serif'],
-                'serif': ['Merriweather', 'serif'],
-                'mono': ['Menlo', 'monospace'],
-                'display': ['Oswald'],
-                'body': ['Inter'],
-                'roboto': ['Roboto'],
-            }
-        },
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './layouts/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: colors.emerald,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        purple: colors.violet,
+        pink: colors.pink,
+        rose: colors.rose,
+        bgLeft: '#984D38',
+        bgRight: '#181E41',
+        primaryDark: '#671AE4',
+        primaryLight: '#B75CFF',
+      },
+      screens: {
+        xs: '320px',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        mono: ['Menlo', 'monospace'],
+        display: ['Oswald'],
+        body: ['Inter'],
+        roboto: ['Roboto'],
+      },
     },
-    plugins: [require("@tailwindcss/typography")],
-};
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+}
